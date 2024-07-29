f90741dc1f407257a22d013893b4f14bdee62a0c
Carlos Pardo <48335533+CarlosPardo98@users.noreply.github.com>
2024-07-29 10:37:21 +0200

    Add files via upload

---
+++ Test/src/main/java/org/example/BillBuilder.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BillBuilder {
+
+    // TODO: Create a Builder to create Bill instances programmatically
+
+    /*
+    In this extra exercise you will implement your first design pattern: a Builder.
+    I don't expect you to know what a Builder is, so you will need to read carefully
+    the pattern specification in Refactoring Guru:
+
+                https://refactoring.guru/design-patterns/builder.
+
+    1. Understand the pattern.
+    2. Read examples.
+    3. Apply the pattern.
+    4. Test it.
+
+    You will have to explain your solution to the team once it is ready. Good luck! ;-D
+     */
+
+}
---
+++ Test/src/main/java/org/example/BillUtils.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.math.BigDecimal;
+
+public class BillUtils {
+
+    /*
+    In this exercise you have to create a type of class that is really common in OOP: a utility class.
+
+    The first thing you will need to do is to understand what a utility class is and why they exist:
+
+                https://www.baeldung.com/java-helper-vs-utility-classes#java-utility-classes
+
+    Once everything is clear, you will have to:
+
+    1. Build the utility class appropriately.
+    2. Implement different functions that perform operation with Bill objects.
+     */
+
+    // TODO: Prevent this class from being instantiated - HINTS: Inheritance? Exposed constructors?
+
+    // Useful documentation: https://www.educative.io/answers/how-to-enforce-non-instantiability-in-a-class-in-java
+
+    // TODO: Create a function that receives a Bill instance and returns the total amount of its products [ITERATIVE]
+
+
+    // TODO: Create a function that receives a Bill instance and returns the total amount of its products [STREAMS]
+
+
+    // TODO: Create a function that receives a Bill instance and returns the number of products that start with the letter A [ITERATIVE]
+
+
+    // TODO: Create a function that receives a Bill instance and returns the number of products that start with the letter A [ITERATIVE]
+
+}
