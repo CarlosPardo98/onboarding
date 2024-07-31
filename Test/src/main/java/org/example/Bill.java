1cedf9aa1e65cc61c3f8443ca937767c192c9321
David Sousa <dsousa@copyright.com>
2024-07-31 10:30:55 +0200

    Bill and BillUtil classes TODO's done

--- Test/src/main/java/org/example/Bill.java
+++ Test/src/main/java/org/example/Bill.java
@@ -11,18 +11,47 @@ public class Bill {
     This class will be used in the rest of the project.
      */
 
-    // TODO: Add instance variables
+    // Add instance variables
+    public String customer;
+    public Map<String, BigDecimal> products;
 
+    // Implement two constructors (default [no params] and with params [HINT: Look at the tests ;-D])
 
-    // TODO: Implement two constructors (default [no params] and with params [HINT: Look at the tests ;-D])
+    public Bill(){}
 
+    public  Bill(String customer, Map<String, BigDecimal> products){
+        this.customer = customer;
+        this.products = products;
+    }
 
-    // TODO: Implement getters and setters
+    // Implement getters and setters
 
+    public String getCustomer(){
+      return this.customer;
+    }
 
+    public void setCustomer(String customer){
+        this.customer = customer;
+    }
 
-    // TODO: Override toString - Expected output:
+    public Map<String, BigDecimal> getProducts(){
+        return this.products;
+    }
 
+    public void setProducts(Map<String, BigDecimal> products){
+        this.products = products;
+    }
+
+    // Override toString - Expected output:
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        res.append("Customer: ").append(getCustomer()).append("\n\n");
+        for(String product: getProducts().keySet()){
+            String productString = product + ": $" + getProducts().get(product) + "\n";
+            res.append(productString);
+        }
+        return String.valueOf(res);
+    }
     /*
     Customer: John Doe
 
--- Test/src/main/java/org/example/BillUtils.java
+++ Test/src/main/java/org/example/BillUtils.java
@@ -1,8 +1,9 @@
 package org.example;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
-public class BillUtils {
+public final class BillUtils {
 
     /*
     In this exercise you have to create a type of class that is really common in OOP: a utility class.
@@ -17,19 +18,41 @@ public class BillUtils {
     2. Implement different functions that perform operation with Bill objects.
      */
 
-    // TODO: Prevent this class from being instantiated - HINTS: Inheritance? Exposed constructors?
+    // Prevent this class from being instantiated - HINTS: Inheritance? Exposed constructors?
+    private BillUtils(){
+        throw new UnsupportedOperationException("Class Instantiation not supported");
+    }
 
     // Useful documentation: https://www.educative.io/answers/how-to-enforce-non-instantiability-in-a-class-in-java
 
-    // TODO: Create a function that receives a Bill instance and returns the total amount of its products [ITERATIVE]
+    // Create a function that receives a Bill instance and returns the total amount of its products [ITERATIVE]
+    public static BigDecimal calculateTotalAmountUsingIterativeModel(Bill bill){
+        BigDecimal totalAmount = new BigDecimal("0.0");
+        for(BigDecimal amount : bill.getProducts().values()){
+            totalAmount = totalAmount.add(amount);
+        }
+        return totalAmount;
+    }
 
+    // Create a function that receives a Bill instance and returns the total amount of its products [STREAMS]
+    public static BigDecimal calculateTotalAmountUsingStreamsBasedModel(Bill bill){
+        BigDecimal totalAmount = BigDecimal.valueOf(0.0);
+        return bill.getProducts().values().stream().reduce(totalAmount, BigDecimal::add);
+    }
 
-    // TODO: Create a function that receives a Bill instance and returns the total amount of its products [STREAMS]
+    // Create a function that receives a Bill instance and returns the number of products that start with the letter A [ITERATIVE]
+    public static long calculateNumberOfProductsThatStartWithAUsingIterativeModel(Bill bill){
+        long numberOfProductsStartingWithA = 0;
+        for (String product: bill.getProducts().keySet()){
+            if(product.startsWith("A")){
+                numberOfProductsStartingWithA += 1;
+            }
+        }
+        return numberOfProductsStartingWithA;
+    }
 
-
-    // TODO: Create a function that receives a Bill instance and returns the number of products that start with the letter A [ITERATIVE]
-
-
-    // TODO: Create a function that receives a Bill instance and returns the number of products that start with the letter A [ITERATIVE]
-
+    // Create a function that receives a Bill instance and returns the number of products that start with the letter A [STREAMS]
+    public static long calculateNumberOfProductsThatStartWithAUsingStreamsBasedModel(Bill bill){
+        return bill.products.keySet().stream().filter(x -> x.startsWith("A")).count();
+    }
 }
