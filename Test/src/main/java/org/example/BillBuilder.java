ce0a6df3ca4252c31dd92a1e6185d6b223bba81f
David Sousa <dsousa@copyright.com>
2024-08-01 09:58:37 +0200

    Added Builder interface and first attempt developing the BillBuilder

--- Test/src/main/java/org/example/BillBuilder.java
+++ Test/src/main/java/org/example/BillBuilder.java
@@ -4,9 +4,40 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
-public class BillBuilder {
+public class BillBuilder implements Builder{
 
-    // TODO: Create a Builder to create Bill instances programmatically
+    // Create a Builder to create Bill instances programmatically
+    private String customer;
+    private Map<String, BigDecimal> products;
+
+    @Override
+    public void setCustomer(String customer) {
+        this.customer=customer;
+    }
+
+    @Override
+    public void setProducts(Map<String, BigDecimal> products) {
+        this.products=products;
+    }
+
+    public Bill getResult(){
+        return new Bill(customer, products);
+    }
+
+    public static void main(String[] args) {
+        String c = "Javier Carretero";
+        Map<String, BigDecimal> p = new HashMap<>();
+        p.put("Cebolla", new BigDecimal("1.4"));
+        p.put("Tomate", new BigDecimal("2.5"));
+        p.put("Pepino", new BigDecimal("3.7"));
+        p.put("Patata", new BigDecimal("5.5"));
+        p.put("Pimienta", new BigDecimal("3.4"));
+        BillBuilder b = new BillBuilder();
+        b.setCustomer(c);
+        b.setProducts(p);
+        Bill bill = b.getResult();
+        System.out.println(bill.toString());
+    }
 
     /*
     In this extra exercise you will implement your first design pattern: a Builder.
