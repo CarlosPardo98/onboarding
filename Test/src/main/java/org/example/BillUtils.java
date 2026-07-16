ceb09655231bd28d6c87b0ff060f850d4f31f9c4
achabrera-ccc <achabrera@copyright.com>
2026-07-16 10:50:46 +0200

    feat: add exercise solution

--- Test/src/main/java/org/example/BillUtils.java
+++ Test/src/main/java/org/example/BillUtils.java
@@ -1,8 +1,10 @@
-package org.example;
+package src.main.java.org.example;
 
 import java.math.BigDecimal;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class BillUtils {
+public final class BillUtils {
 
     /*
     In this exercise you have to create a type of class that is really common in OOP: a utility class.
@@ -21,15 +23,42 @@ public class BillUtils {
 
     // Useful documentation: https://www.educative.io/answers/how-to-enforce-non-instantiability-in-a-class-in-java
 
+    private BillUtils() {
+        throw new UnsupportedOperationException("Class Instantiation not supported");
+    }
+
     // TODO: Create a function that receives a Bill instance and returns the total amount of its products [ITERATIVE]
 
+    public static BigDecimal calculateTotalAmountUsingIterativeModel(Bill bill) {
+        BigDecimal totalAmount = new BigDecimal("0.0");
+        for (BigDecimal productAmount: bill.getProductAndPrices().values()) {
+            totalAmount = totalAmount.add(productAmount);
+        }
+        return totalAmount;
+    }
 
     // TODO: Create a function that receives a Bill instance and returns the total amount of its products [STREAMS]
 
+    public static BigDecimal calculateTotalAmountUsingStreamsBasedModel(Bill bill) {
+        return bill.getProductAndPrices().values().stream().reduce(BigDecimal::add).get();
+    }
 
     // TODO: Create a function that receives a Bill instance and returns the number of products that start with the letter A [ITERATIVE]
 
+    public static Long calculateNumberOfProductsThatStartWithAUsingIterativeModel(Bill bill) {
+        Long amount = 0L;
+        for (String product: bill.getProductAndPrices().keySet()) {
+            if (product.toLowerCase().startsWith("a")) {
+                amount += 1L;
+            }
+        }
+        return amount;
+    }
 
     // TODO: Create a function that receives a Bill instance and returns the number of products that start with the letter A [ITERATIVE]
+
+    public static Long calculateNumberOfProductsThatStartWithAUsingStreamsBasedModel(Bill bill) {
+        return bill.getProductAndPrices().keySet().stream().map(String::toLowerCase).filter(p -> p.startsWith("a")).count();
+    }
 
 }
